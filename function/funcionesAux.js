f59b4ddf0af00a748f1167c951db7b80e45cb395
Nelson Gonzalaez <Nelsongr872@gmailcom>
2024-08-03 01:09:01 -0400

    subiendo cambios del portal

--- function/funcionesAux.js
+++ function/funcionesAux.js
@@ -1,5 +1,5 @@
 import CalcularDistancias from "../class/calcularDistancias.js";
-import { update } from "../js/main.js";
+
 //funciones auxiliares
 //funncion de dibujar fondo
 export const drawBG = (ctx, canvas, color) => {
@@ -8,41 +8,49 @@ export const drawBG = (ctx, canvas, color) => {
 };
 
 //funcion de mecanica de colision
-export const colision = (jugador, enemigo, bonos, portales, nivel) => {
+export const colision = (
+  jugador,
+  enemigosNivel,
+  bonos,
+  portales,
+  gameState
+) => {
   //objetos de la clase Calculardistancias
-  const distancia_je = new CalcularDistancias(jugador, enemigo);
 
-  // mecanica de colision jugador-enemigo teniendo jugador mayor ataque
-  if (enemigo.energia > 0 && jugador.ataque > enemigo.ataque) {
-    if (distancia_je.distancia <= distancia_je.sumaDeRadios) {
-      console.log("colision con el enemigo1");
-      if (enemigo.espada > 0) {
-        enemigo.espada += -jugador.ataque;
-      } else {
-        enemigo.energia += -jugador.ataque;
-      }
+  enemigosNivel.forEach((enemigo) => {
+    const distancia_je = new CalcularDistancias(jugador, enemigo);
+    // mecanica de colision jugador-enemigo teniendo jugador mayor ataque
+    if (enemigo.energia > 0 && jugador.ataque > enemigo.ataque) {
+      if (distancia_je.distancia <= distancia_je.sumaDeRadios) {
+        console.log("colision con el enemigo1");
+        if (enemigo.espada > 0) {
+          enemigo.espada += -jugador.ataque;
+        } else {
+          enemigo.energia += -jugador.ataque;
+        }
 
-      if (enemigo.energia === 0) {
-        console.log("mataste al enemigo");
-      }
-    }
-  }
-
-  // mecanica de colision jugador-enemigo teniendo enemigo mayor ataque
-  if (jugador.energia > 0 && enemigo.ataque > jugador.ataque) {
-    if (distancia_je.distancia <= distancia_je.sumaDeRadios) {
-      console.log("colision con el enemigo");
-      if (jugador.espada > 0) {
-        jugador.espada += -enemigo.ataque;
-      } else {
-        jugador.energia += -enemigo.ataque;
-      }
-
-      if (jugador.energia === 0) {
-        console.log("moriste por el enemigo");
+        if (enemigo.energia === 0) {
+          console.log("mataste al enemigo");
+        }
       }
     }
-  }
+    // mecanica de colision jugador-enemigo teniendo enemigo mayor ataque
+    if (jugador.energia > 0 && enemigo.ataque > jugador.ataque) {
+      if (distancia_je.distancia <= distancia_je.sumaDeRadios) {
+        console.log("colision con el enemigo");
+        if (jugador.espada > 0) {
+          jugador.espada += -enemigo.ataque;
+        } else {
+          jugador.energia += -enemigo.ataque;
+        }
+
+        if (jugador.energia === 0) {
+          console.log("moriste por el enemigo");
+        }
+      }
+    }
+  });
+
   // mecanica de colisión entre jugador y bonos
   bonos.forEach((bono) => {
     const distancia_jb = new CalcularDistancias(jugador, bono);
@@ -63,22 +71,32 @@ export const colision = (jugador, enemigo, bonos, portales, nivel) => {
   //mecanica de colision entre jugador y portal
   portales.forEach((portal) => {
     const distancia_jp = new CalcularDistancias(jugador, portal);
-
     if (distancia_jp.distancia <= distancia_jp.sumaDeRadios) {
       if (portal.numero == 1) {
-        //ir a hacia el nivel dos
-        nivel = 2;
-        console.log("colisione con el portal", " nivel -->", nivel);
+        // Ir hacia el nivel dos
+        gameState.nivel = 2;
+        console.log("Colisión con el portal", "nivel -->", gameState.nivel);
       } else if (portal.numero == 2) {
-        //ir hacia el nivel uno
-        nivel = 1;
+        // Ir hacia el nivel uno
+        gameState.nivel = 1;
       } else if (portal.numero == 3) {
-        //ir hacia el nivel tres
-        nivel = 3;
+        // Ir hacia el nivel tres
+        gameState.nivel = 3;
       } else if (portal.numero == 4) {
-        //ir hacia el nivel dos
-        nivel = 2;
+        // Ir hacia el nivel dos
+        gameState.nivel = 2;
       }
     }
   });
+};
+
+//dibujar estrellas
+export const drawStars = (number = 40, ctx, canvas) => {
+  for (let i = 0; i < number; i++) {
+    let x = Math.floor(Math.random() * canvas.width);
+    let y = Math.floor(Math.random() * canvas.height);
+    ctx.fillStyle = "white";
+    ctx.fillRect(x, y, 1, 3);
+    ctx.fillRect(--x, ++y, 3, 1);
+  }
 };
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// main.js
+
 //importar clases
 import Enemigo from "../class/enemigo.js";
 import Guerrero from "../class/guerrero.js";
@@ -5,13 +7,13 @@ import Bono from "../class/bono.js";
 import Portal from "../class/portal.js";
 
 //Importar funciones auxiliares
-import { drawBG, colision } from "../function/funcionesAux.js";
+import { drawBG, colision, drawStars } from "../function/funcionesAux.js";
 
 //Importar constantes
 import { ctx, canvas, keys } from "../constants/constants.js";
 
 //importar variables
-import { nivel } from "../variables/variables.js";
+import { gameState } from "../variables/variables.js";
 
 //Agregar el elemento canvas al html y definir los atributos de ancho y alto
 document.body.appendChild(canvas);
@@ -34,48 +36,56 @@ const warrior = new Guerrero(
   75,
   "./../img/Sword.png"
 );
-const enemie1 = new Enemigo(
-  canvas.width / 10.5,
-  canvas.height / 6.5,
-  74,
-  64,
-  70,
-  10,
-  keys,
-  ctx,
-  1000,
-  200,
-  2,
-  canvas,
-  "./../img/enemigos/enemigo1.png",
-  75,
-  "./../img/enemigos/espada1.png",
-  2,
-  2
-);
-const enemie5 = new Enemigo(
-  canvas.width / 10.5,
-  canvas.height / 6.5,
-  74,
-  64,
-  70,
-  10,
-  keys,
-  ctx,
-  1000,
-  200,
-  2,
-  canvas,
-  "./../img/enemigos/enemigo5.png",
-  75,
-  "./../img/enemigos/espada5.png",
-  2,
-  2
-);
-const bonusList = [
+//array de enemigos segun su nivel
+let enemigoNivel1 = [
+  new Enemigo(
+    canvas.width / 10.5,
+    canvas.height / 6.5,
+    74,
+    64,
+    70,
+    10,
+    keys,
+    ctx,
+    1000,
+    200,
+    2,
+    canvas,
+    "./../img/enemigos/enemigo1.png",
+    75,
+    "./../img/enemigos/espada1.png",
+    2,
+    2
+  ),
+];
+let enemigoNivel2 = [
+  new Enemigo(
+    canvas.width / 10.5,
+    canvas.height / 6.5,
+    74,
+    64,
+    70,
+    10,
+    keys,
+    ctx,
+    1000,
+    200,
+    2,
+    canvas,
+    "./../img/enemigos/enemigo5.png",
+    75,
+    "./../img/enemigos/espada5.png",
+    2,
+    2
+  ),
+];
+let enemigoNivel3 = [];
+
+//array de bonos segun su nivel
+let bonusNivel1 = [
   new Bono(
     warrior.position.x,
-    enemie1.position.y + 100,
+    enemigoNivel1[0].position.y + 100,
     75,
     75,
     "./../img/bono/bono13.png",
@@ -83,55 +93,153 @@ const bonusList = [
     30
   ),
 ];
-const portales = [
+let bonusNivel2 = [
+  new Bono(
+    warrior.position.x,
+    enemigoNivel1[0].position.y + 100,
+    75,
+    75,
+    "./../img/bono/bono13.png",
+    "espada",
+    30
+  ),
+];
+let bonusNivel3 = [
+  new Bono(
+    warrior.position.x,
+    enemigoNivel1[0].position.y + 100,
+    75,
+    75,
+    "./../img/bono/bono13.png",
+    "espada",
+    30
+  ),
+];
+
+//array de portales segun su nivel
+let portalesNivel1 = [
   new Portal(
     warrior.position.x - 50,
-    enemie1.position.y - 100,
+    enemigoNivel1[0].position.y - 100,
     150,
     150,
     50,
     1
   ),
+];
+let portalesNivel2 = [
   new Portal(
     warrior.position.x - 50,
-    enemie1.position.y + 400,
+    enemigoNivel1[0].position.y + 400,
+    150,
+    150,
+    50,
+    2
+  ),
+  new Portal(
+    warrior.position.x - 50,
+    enemigoNivel1[0].position.y + 400,
     150,
     150,
     50,
     2
   ),
 ];
+let portalesNivel3 = [
+  new Portal(
+    warrior.position.x - 50,
+    enemigoNivel1[0].position.y - 100,
+    150,
+    150,
+    50,
+    1
+  ),
+];
+let nivelAnterior = gameState.nivel;
+
+let enemigosNivel1R = enemigoNivel1;
+let bonusNivel1R = bonusNivel1;
+let portalesNivel1R = portalesNivel1;
+
+let enemigosNivel2R = enemigoNivel2;
+let bonusNivel2R = bonusNivel2;
+let portalesNivel2R = portalesNivel2;
+
+let enemigosNivel3R = enemigoNivel3;
+let bonusNivel3R = bonusNivel3;
+let portalesNivel3R = portalesNivel3;
 //Disparador
 export const update = () => {
   //
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  //
+  console.log(enemigoNivel1);
+  drawBG(ctx, canvas, "#121212");
+  drawStars(20, ctx, canvas);
+  if (gameState.nivel !== nivelAnterior) {
+    nivelAnterior = gameState.nivel;
 
-  if (nivel == 1) {
-    drawBG(ctx, canvas, "#1f5705");
-    warrior.update();
-    enemie1.update();
-    bonusList.forEach((bono) => bono.draw(ctx));
-    portales.forEach((portal) => {
-      if (portal.numero == 1) {
-        portal.draw(ctx);
-      }
-    });
-    colision(warrior, enemie1, bonusList, portales);
-  } else if (nivel == 2) {
-    drawBG(ctx, canvas, "#323232");
-    console.log("estoy dentro del nivel 2 update");
-    warrior.update();
-    enemie5.update();
-    bonusList.forEach((bono) => bono.draw(ctx));
-    portales.forEach((portal) => {
-      if (portal.numero == 1) {
-        portal.draw(ctx);
-      }
-    });
-    colision(warrior, enemie5, bonusList, portales);
+    // Reposicionar al guerrero en el centro del canvas
+    warrior.position.x = canvas.width / 2 - warrior.w / 2;
+    warrior.position.y = canvas.height / 2 - warrior.h / 2;
   }
-  //
+
+  // Detectar el nivel para renderizar los enemigos y portales correspondientes
+  if (gameState.nivel === 1) {
+    enemigoNivel1 = enemigosNivel1R;
+    bonusNivel1 = bonusNivel1R;
+    portalesNivel1 = portalesNivel1R;
+
+    enemigoNivel2 = [];
+    bonusNivel2 = [];
+    portalesNivel2 = [];
+
+    enemigoNivel3 = [];
+    bonusNivel3 = [];
+    portalesNivel3 = [];
+
+    warrior.update();
+    enemigoNivel1.forEach((enemigo) => enemigo.draw(ctx));
+    bonusNivel1.forEach((bonus) => bonus.draw(ctx));
+    portalesNivel1.forEach((portal) => portal.draw(ctx));
+    colision(warrior, enemigoNivel1, bonusNivel1, portalesNivel1, gameState);
+  } else if (gameState.nivel === 2) {
+    enemigoNivel2 = enemigosNivel2R;
+    bonusNivel2 = bonusNivel2R;
+    portalesNivel2 = portalesNivel2R;
+
+    enemigoNivel1 = [];
+    bonusNivel1 = [];
+    portalesNivel1 = [];
+
+    enemigoNivel3 = [];
+    bonusNivel3 = [];
+    portalesNivel3 = [];
+
+    warrior.update();
+    enemigoNivel2.forEach((enemigo) => enemigo.draw(ctx));
+    bonusNivel2.forEach((bonus) => bonus.draw(ctx));
+    portalesNivel2.forEach((portal) => portal.draw(ctx));
+    colision(warrior, enemigoNivel2, bonusNivel2, portalesNivel2, gameState);
+  } else if (gameState.nivel === 3) {
+    enemigoNivel3 = enemigosNivel3R;
+    bonusNivel3 = bonusNivel3R;
+    portalesNivel3 = portalesNivel3R;
+
+    enemigoNivel1 = [];
+    bonusNivel1 = [];
+    portalesNivel1 = [];
+
+    enemigoNivel2 = [];
+    bonusNivel2 = [];
+    portalesNivel2 = [];
+
+    warrior.update();
+    enemigoNivel3.forEach((enemigo) => enemigo.draw(ctx));
+    bonusNivel3.forEach((bonus) => bonus.draw(ctx));
+    portalesNivel3.forEach((portal) => portal.draw(ctx));
+    colision(warrior, enemigoNivel3, bonusNivel3, portalesNivel3, gameState);
+  }
+
   requestAnimationFrame(update);
 };
 
